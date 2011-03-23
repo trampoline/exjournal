6c5103e5f92b87b96f2f0c8011042df372ea9980
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2011-03-23 16:25:30 +0000

    - parse original messages from exchange journal messages - encode message metadata in a JSON compatible format

---
+++ lib/exjournal.rb
@@ -0,0 +1,83 @@
+require 'action_mailer'
+require 'tmail'
+
+module Exjournal
+  module_function
+  
+  # extract the first message/rfc822 attachment from the RFC822 encoded content, and return it as a TMail::Mail
+  # if +headers_only+ is true then message content will be discarded, and only headers processed
+  def extract_journalled_mail(mail, strip_content=true)
+    journal_mail = TMail::Mail.parse(mail) if mail.is_a?(String)
+    
+    # get the attachment
+    attachment = journal_mail.parts.select{ |p| p.content_disposition == "attachment" || p.content_type == "message/rfc822" }.first
+    
+    # complain if the email has no attachment to extract
+    raise "attempted to extract journalled mail, but message has no attachments: \n#{mail}\n\n" unless attachment
+
+    mail_content = strip_content ? discard_mail_body(attachment.body) : attachment.body
+
+    TMail::Mail.parse(mail_content)
+  end
+
+  # discard everything after the first \n\n , i.e. all message body content from an RFC822 encoded mail
+  def discard_mail_body(content)
+    content.gsub(/^(.*?)\r\n\r\n.*$/m, '\1')
+  end
+
+  # remove angle brackets from a header string
+  def strip_header(header)
+    header.gsub(/^<(.*)>$/, '\1')
+  end
+
+  def with_headers(headers)
+    [*(headers||[])].map{|h| yield h}
+  end
+
+  # remove angle brackets from one or more headers
+  def strip_headers(headers)
+    with_headers(headers){|h| strip_header(h)}
+  end
+
+  # parse an address to a hash
+  def parse_address(address)
+    address = TMail::Address.parse(address) if address.is_a?(String)
+    {:name=>address.name, :address=>address.address}
+  end
+
+  # parse one or more addresses to hash
+  def parse_addresses(addresses)
+    [*(addresses||[])].map{|a| parse_address(a)}
+  end
+
+  # turn a TMail::Mail into a has suitable for JSON representation
+  def mail_to_hash(mail, strip_content=true)
+    mail = TMail::Mail.parse(mail) if mail.is_a?(String)
+
+    message_id = strip_header(mail.message_id) if mail.message_id
+    sent_at = mail.date.xmlschema
+    in_reply_to = strip_headers(mail.in_reply_to) if mail.in_reply_to
+    references = strip_headers(mail.references) if mail.references
+    from = parse_addresses(mail.from_addrs).first
+    to = parse_addresses(mail.to_addrs)
+    cc=parse_addresses(mail.cc_addrs)
+    bcc=parse_addresses(mail.bcc_addrs)
+
+    h = {
+      :message_id=>message_id,
+      :sent_at=>sent_at,
+      :in_reply_to=>in_reply_to,
+      :references=>references,
+      :from=>from,
+      :to=>to,
+      :cc=>cc,
+      :bcc=>bcc
+    }
+
+    if !strip_content
+      h[:subject] = mail.subject
+    end
+
+    h
+  end
+end
--- spec/exjournal_spec.rb
+++ spec/exjournal_spec.rb
@@ -1,7 +1,127 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Exjournal" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  describe "extract_journalled_mail" do
+    it "should extract the first message/rfc822 attachment without content" do
+      s=File.read(File.expand_path("../fixtures/email_with_email_attachment.eml", __FILE__))
+      jm = Exjournal.extract_journalled_mail(s)
+      jm.message_id.should == "<0FDFBAF6-0960-40FB-B730-B0C7C3299948@empire42.com>"
+      jm.body.should =~ /\s*/
+    end
+
+    it "should extract the first message/rfc822 attachment with content" do
+      s=File.read(File.expand_path("../fixtures/email_with_email_attachment.eml", __FILE__))
+      jm = Exjournal.extract_journalled_mail(s, false)
+      jm.message_id.should == "<0FDFBAF6-0960-40FB-B730-B0C7C3299948@empire42.com>"
+      jm.body.should !~ /\s*/
+    end
+
+    it "should raise if there is no message/rfc822 attachment" do
+      s=File.read(File.expand_path("../fixtures/email_without_attachment.eml", __FILE__))
+      lambda{
+        Exjournal.extract_journalled_mail(s)
+      }.should raise_error(/has no attachments/)
+    end
+  end
+
+  describe "discard_mail_body" do
+    it "should discard everything after \r\n\r\n" do
+      s=<<-EOF
+From: foo@bar.com\r
+To: baz@boo.com\r
+Subject: i can't tell you\r
+\r
+because it's a secret\r
+      EOF
+      Exjournal.discard_mail_body(s).should =~ /^From: foo@bar.com.*tell you$/m
+    end
+
+    it "should do nothing if no \r\n\r\n" do
+      s=<<-EOF
+From: foo@bar.com\r
+To: baz@boo.com\r
+Subject: i can't tell you
+      EOF
+      Exjournal.discard_mail_body(s).should == s
+    end
+  end
+
+  describe "strip_header" do
+    it "should strip angle brackets if present" do
+      Exjournal.strip_header("<foo>").should == "foo"
+    end
+
+    it "should do nothing if no angle brackets" do
+      Exjournal.strip_header("foo").should == "foo"
+    end
+  end
+
+  describe "strip_headers" do
+    it "should strip_header from a single header" do
+      Exjournal.strip_headers("<foo>").should == ["foo"]
+    end
+
+    it "should strip_header from more than one header" do
+      Exjournal.strip_headers(["<foo>", "<bar>"]).should == ["foo", "bar"]
+    end
+  end
+
+  describe "parse_address" do
+    it "should extract a hash from an address" do
+      h = Exjournal.parse_address(TMail::Address.parse('"foo mcfoo" <foo@bar.com>'))
+      h.should == {:name=>"foo mcfoo", :address=>"foo@bar.com"}
+    end
+  end
+
+  describe "parse_addresses" do
+    it "should extract a hash from a single address" do
+      h = Exjournal.parse_addresses(TMail::Address.parse('"foo mcfoo" <foo@bar.com>'))
+      h.should == [{:name=>"foo mcfoo", :address=>"foo@bar.com"}]
+    end
+
+    it "should extract hashes from more than one address" do
+      h = Exjournal.parse_addresses([TMail::Address.parse('"foo mcfoo" <foo@bar.com>'),
+                                     TMail::Address.parse('"baz mcbaz" <baz@boo.com>')])
+      h.should == [{:name=>"foo mcfoo", :address=>"foo@bar.com"},
+                   {:name=>"baz mcbaz", :address=>"baz@boo.com"}]
+    end
+  end
+
+  describe "mail_to_hash" do
+    it "should turn a mail to json excluding subject" do
+      s=File.read(File.expand_path("../fixtures/email_with_references.eml", __FILE__))    
+      h = Exjournal.mail_to_hash(s)
+
+      h[:message_id].should == "B63CAA43-F378-4033-BA8B-ED408805B5B5@empire42.com"
+      h[:sent_at].should == DateTime.parse('Wed, 21 Apr 2010 14:43:24 +0100').xmlschema
+      h[:in_reply_to].should == ["B63CAA43-F378-4033-BA8B-foo@empire42.com"]
+      h[:references].should == ["B63CAA43-F378-4033-BA8B-foo@empire42.com", 
+                                "B63CAA43-F378-4033-BA8B-bar@empire42.com", 
+                                "B63CAA43-F378-4033-BA8B-baz@empire42.com"]
+      h[:from].should == {:name=>"Peter MacRobert", :address=>"peter.macrobert@empire42.com"}
+      h[:to].should == [{:name=>nil, :address=>"foo@bar.com"},
+                        {:name=>"bar mcbar", :address=>"bar.mcbar@bar.com"}]
+      h[:cc].should == []
+      h[:bcc].should == []
+      h[:subject].should == nil
+    end
+
+    it "should turn a mail to json including subject" do
+      s=File.read(File.expand_path("../fixtures/email_with_references.eml", __FILE__))    
+      h = Exjournal.mail_to_hash(s, false)
+
+      h[:message_id].should == "B63CAA43-F378-4033-BA8B-ED408805B5B5@empire42.com"
+      h[:sent_at].should == DateTime.parse('Wed, 21 Apr 2010 14:43:24 +0100').xmlschema
+      h[:in_reply_to].should == ["B63CAA43-F378-4033-BA8B-foo@empire42.com"]
+      h[:references].should == ["B63CAA43-F378-4033-BA8B-foo@empire42.com", 
+                                "B63CAA43-F378-4033-BA8B-bar@empire42.com", 
+                                "B63CAA43-F378-4033-BA8B-baz@empire42.com"]
+      h[:from].should == {:name=>"Peter MacRobert", :address=>"peter.macrobert@empire42.com"}
+      h[:to].should == [{:name=>nil, :address=>"foo@bar.com"},
+                        {:name=>"bar mcbar", :address=>"bar.mcbar@bar.com"}]
+      h[:cc].should == []
+      h[:bcc].should == []
+      h[:subject].should == "Test email"
+    end
   end
 end
